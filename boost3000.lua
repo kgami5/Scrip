bd8331d030f4a73c89dac4a69b67394c386f15d4
kgami5 <129633143+kgami5@users.noreply.github.com>
2025-09-30 04:27:27 +0200

    boost3000.lua

--- boost3000.lua
+++ boost3000.lua
@@ -8,8 +8,8 @@ import "layout"
 --activity.setTheme(R.Theme_Blue)
 --activity.setTitle("MyApp2")
 activity.overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
-activity.ActionBar.hide()
-activity.ActionBar.setElevation(0)
+--activity.ActionBar.hide()
+--activity.ActionBar.setElevation(0)
 activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 activity.setContentView(loadlayout(layout))
 activity.setRequestedOrientation(1)
